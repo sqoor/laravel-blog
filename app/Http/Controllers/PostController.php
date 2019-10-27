53695cb73de5d29b6a31fadaa9dcb6ed25163af8
sqoor <alsqoorAbdullah@gmail.com>
2019-10-27 06:44:15 +0200

    added access control to the routes that requires authenticated users'

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,17 @@ use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show', '']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
